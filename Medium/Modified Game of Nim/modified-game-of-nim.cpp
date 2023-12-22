e3256206a4efe0a50d5f372eb37ab739b308c2ac
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2023-12-22 23:31:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modified Game of Nim/modified-game-of-nim.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        int x = 0;
+        for(int i = 0 ; i < n ; i++){
+            x ^= A[i];
+        }
+        if(x == 0 || n % 2 == 0){
+            return 1;
+        }
+        return 2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.findWinner(n, A)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
