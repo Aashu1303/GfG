91e4cc0fccbf569f6c14028bbe3453166914bb2e
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-02-24 15:40:49 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Sum Problem/maximum-sum-problem.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int maxSum(int n)
+        {
+            if(n == 0) return 0;
+            return max(n , (maxSum(n/2)+maxSum(n/3)+maxSum(n/4)));
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.maxSum(n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
