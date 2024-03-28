7ef232ba61726feec9b29d0c9b637434c49140ca
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-03-28 11:49:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find shortest safe route in a matrix/find-shortest-safe-route-in-a-matrix.cpp
+++ Medium/Find shortest safe route in a matrix/find-shortest-safe-route-in-a-matrix.cpp
@@ -8,57 +8,61 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+#include <vector>
+#include <climits>
+using namespace std;
+
+int r , c;
+int dp[1001][1001];
+bool visited[1001][1001];
+
+
 class Solution
 {
-private:
-    vector<int> X = {-1,0,1};
-    vector<int> Y = {0,1,0};
-    
-    bool isBoundery(int i,int j,int n,int m){
-        return (i>=0 && j>=0 && i<n && j<m);
+    bool check(int i, int j, vector<vector<int>> &mat)
+    {
+        if (i - 1 >= 0 && mat[i - 1][j] == 0) return false;
+        if (i + 1 < mat.size() && mat[i + 1][j] == 0) return false;
+        if (j + 1 < mat[0].size() && mat[i][j + 1] == 0) return false;
+        if (j - 1 >= 0 && mat[i][j - 1] == 0) return false;
+
+        return true;
     }
+
+    int dfs(int i, int j, vector<vector<int>> &mat)
+    {
+        if (i < 0 || j < 0 || i >= mat.size() || j >= mat[0].size() || visited[i][j] || mat[i][j] == 0) return 100000;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (!check(i, j, mat)) return 100000;
+        if (j == 0) return mat[i][0];
+
+        visited[i][j] = true;
+
+        int left = mat[i][j] + dfs(i, j - 1, mat);
+        int down = mat[i][j] + dfs(i + 1, j, mat);
+        int top = mat[i][j] + dfs(i - 1, j, mat);
+
+        visited[i][j] = false;
+
+        return dp[i][j] = min(min(left, top), down);
+    }
+
 public:
-    int findShortestPath(vector<vector<int>> &mat){
-        int r=mat.size(),c=mat[0].size();
-        for(int i=0; i<r;i++){
-            for(int j=0; j<c;j++){
-                if(mat[i][j]==0){
-                    if(i-1>=0)mat[i-1][j]=-1;
-                    if(i+1<r)mat[i+1][j]=-1;
-                    if(j-1>=0)mat[i][j-1]=-1;
-                    if(j+1<c)mat[i][j+1]=-1;
-                    
-                }
-           }
-       }
-       
-       vector<vector<int>> dp(r,vector<int>(c,INT_MAX));
-       priority_queue<pair<int,pair<int,int>>,
-                    vector<pair<int,pair<int,int>>>,
-                    greater<pair<int,pair<int,int>>>> q;
-       
-        for(int i=0; i<r; i++)
-            if(mat[i][0]==1) q.push({1,{i,0}});
+    int findShortestPath(vector<vector<int>> &mat)
+    {
+        r = mat.size();
+        c = mat[0].size();
         
-        while(!q.empty()){
-            auto curr = q.top();
-                        q.pop();
-            int steps=curr.first;
-            int i=curr.second.first;
-            int j=curr.second.second;
-            
-            if(j==c-1) return steps;
-            
-            for(int k=0; k<3; k++){
-                int newX = i+X[k];
-                int newY = j+Y[k];
-                if(isBoundery(newX,newY,r,c) && mat[newX][newY]==1 && dp[newX][newY]>steps+1){
-                    q.push({steps+1,{newX,newY}});
-                    dp[newX][newY] = steps+1;
-                }
-            }
+        memset(dp,-1,sizeof dp);
+        memset(visited,false,sizeof visited);
+
+        int ans = 100000;
+        for (int i = 0; i < r; i++) {
+            ans = min(ans, dfs(i, c - 1, mat));
         }
-        return -1;
+
+        return ans == 100000 ? -1 : ans;
     }
 };
 
