f5e05a902fbaa3e5888f37dd499d6b3b00f8aeda
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-04-29 21:53:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove every kth node/remove-every-kth-node.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    
+    *tail_ref = new_node;
+}
+
+
+// } Driver Code Ends
+/* Link list Node 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+
+/*You are required to complete this method*/
+
+/* Function to get the middle of the linked list*/
+/*K will always be in range */
+
+class Solution {
+    public:
+    Node* deleteK(Node *head,int K){
+        if(K == 1 || head -> next == NULL || head == NULL) return NULL;
+        //Your code here
+        int x = 1; Node* curr = head , *prev = NULL;
+        while(curr != NULL){
+            if(x % K == 0){
+                prev -> next = curr -> next;
+            }
+            x++;
+            prev = curr;
+            curr = curr -> next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+        int K;
+        cin>>K;
+        Solution obj;
+        Node *res = obj.deleteK(head,K);
+        Node *temp = res;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
